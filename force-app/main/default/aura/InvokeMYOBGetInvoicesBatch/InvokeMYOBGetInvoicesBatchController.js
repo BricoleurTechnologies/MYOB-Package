18fe018becdf6ed6504bce5a79a75864fb1a14d1
Dekshant Khandelwal <dekshant.khandelwal@crmit.com>
2024-11-13 14:18:06 +0530

    MYOB Package

---
+++ force-app/main/default/aura/InvokeMYOBGetInvoicesBatch/InvokeMYOBGetInvoicesBatchController.js
@@ -0,0 +1,31 @@
+({
+	 doInit : function(component, event, helper) {
+        var action =component.get('c.runMYOBGetInvoicesBatch');
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                $A.get("e.force:closeQuickAction").fire();
+            }
+            else if (state === "INCOMPLETE") {
+                $A.get("e.force:closeQuickAction").fire();
+            }
+                else if (state === "ERROR") {
+                    var errors = response.getError();
+                    if (errors) {
+                        if (errors[0] && errors[0].message) {
+                            console.log("Error message: " + 
+                                        errors[0].message);
+                        }
+                    } else {
+                        console.log("Unknown error");
+                    }
+                    
+                    $A.get("e.force:closeQuickAction").fire();
+                }
+        });
+        
+        $A.enqueueAction(action);
+        
+    }
+})
